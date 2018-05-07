7f4cd1c6f40c9cfd2dd3c917fce14af186a968f7
Xcandie44 <pechnikov.ivan@yandex.by>
2018-05-07 11:15:57 +0300

    Cat

--- app/src/main/java/com/example/user/project/MainActivity.java
+++ app/src/main/java/com/example/user/project/MainActivity.java
@@ -3,13 +3,52 @@ package com.example.user.project;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
+
 
 public class MainActivity extends AppCompatActivity{
+    private TextView mInfoTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mInfoTextView = (TextView) findViewById(R.id.textView);
+    }
 
+    public void onClick(View view) {
+        send send = new send();
+        send.execute();
+    }
+
+
+
+    class send extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mInfoTextView.setText("Полез на крышу");
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            mInfoTextView.setText("Залез");
+        }
     }
 }
