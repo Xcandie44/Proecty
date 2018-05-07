dc0e6a768684d00f59264266a24ccd2774fa31ef
Xcandie44 <pechnikov.ivan@yandex.by>
2018-05-07 10:51:10 +0300

    Тест

--- app/src/main/java/com/example/user/project/MainActivity.java
+++ app/src/main/java/com/example/user/project/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.user.project;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       //43434354
+
     }
 }
